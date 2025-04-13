e87096b7e13fdf0b7a879ed2b8c9460de1de6ace
Yeonwoo2017 <kiteu23@gmail.com>
2025-04-13 15:13:52 +0900

    고객 컴포넌트 구조화 #2

--- src/App.js
+++ src/App.js
@@ -2,24 +2,55 @@ import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-const customer = {
+const customers = [{
+  'id': 1,
+  'image': 'https://picsum.photos/64/64?random=1',
   'name': '김철수',
-  'birthday' : '981231',
-  'gender' : '남자',
-  'job' : '대학생'
+  'birthday': '981231',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://picsum.photos/64/64?random=2',
+  'name': '홍길동',
+  'birthday': '961122',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 3,
+  'image': 'https://picsum.photos/64/64?random=3',
+  'name': '김혜자',
+  'birthday': '921122',
+  'gender': '여자',
+  'job': '대학생'
 }
 
+]
+
 class App extends Component {
-  render(){
+  render() {
     return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
-   );
- }
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
