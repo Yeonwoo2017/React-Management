98c4cb89f83beb00f0e10de8421127eed56eab9d
Yeonwoo2017 <kiteu23@gmail.com>
2025-04-24 19:10:22 +0900

    서버개발환경구축#4

---
+++ client/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+import Customer from './components/Customer';
+import {
+  Paper,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@mui/material";
+import { styled } from "@mui/system";
+
+const StyledPaper = styled(Paper)({
+  width: "100%",
+  marginTop: "24px",
+  overflow: "auto",
+});
+
+const StyledTable = styled(Table)({
+  minWidth: 1080,
+});
+
+function App() {
+  const [customers, setCustomers] = useState("");
+
+  useEffect(() => {
+    const callApi = async () => {
+      const response = await fetch("/api/customers");
+      const body = await response.json();
+      return body;
+    };
+
+    callApi()
+      .then((res) => setCustomers(res))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <StyledPaper>
+      <StyledTable>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers
+            ? customers.map((c) => (
+                <Customer
+                  key={c.id} // map 을 사용하려면 key 라는 속성이 있어야 함(안하면 Console창에 에러가 발생)
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              ))
+            : ""}
+        </TableBody>
+      </StyledTable>
+    </StyledPaper>
+  );
+}
+export default App;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+        'id': 1,
+        'image': 'https://picsum.photos/64/64?random=1',
+        'name': '김철수',
+        'birthday': '981231',
+        'gender': '남자',
+        'job': '학생'
+      },
+      {
+        'id': 2,
+        'image': 'https://picsum.photos/64/64?random=2',
+        'name': '홍길동',
+        'birthday': '961122',
+        'gender': '남자',
+        'job': '트레이너'
+      },
+      {
+        'id': 3,
+        'image': 'https://picsum.photos/64/64?random=3',
+        'name': '김혜자',
+        'birthday': '921122',
+        'gender': '여자',
+        'job': '요리사'
+      },
+  ]);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
