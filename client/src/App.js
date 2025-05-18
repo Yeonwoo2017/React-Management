1a656360a42d9bcc3f411885c2aa00edb1e1c94b
Yeonwoo2017 <kiteu23@gmail.com>
2025-05-18 13:16:21 +0900

    vercel 배포 수정 #7

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,6 @@ import {
 } from "@mui/material";
 import { styled, useTheme } from "@mui/system";
 
-const port = 3000;
-App.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
 
 module.exports = App;
 
